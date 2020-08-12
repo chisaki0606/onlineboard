088ccbbeeabc3acaa95f8019cc9bc122bdb9880b
“chisaki0606” <witness6961@gmail.com>
2020-08-12 09:32:52 +0900

    add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  post "users/create" => "users#create"
+  get "signup" => "users#new"
+  get "users/:id" => "users#show"
+
   get "/" => "posts#index"
   get "posts/new" => "posts#new"
   post "posts/create" => "posts#create"
   get "posts/:id" => "posts#show" 
   get "posts/:id/edit" => "posts#edit"
-  post 'posts/:id/update' => 'posts#update'
-  get 'posts/:id/destroy' => 'posts#destroy'
+  post "posts/:id/update" => "posts#update"
+  get "posts/:id/destroy" => "posts#destroy"
 end
